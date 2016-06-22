844cc9340b109be97020e10864b26844a99a9231
Robert Ramirez <rob@robtherich.org>
2016-06-22 19:40:48 -0400

    jit.mo.gen basic functionality

--- source/projects/jit.mo.gen/jit.mo.gen.cpp
+++ source/projects/jit.mo.gen/jit.mo.gen.cpp
@@ -9,33 +9,55 @@
 using namespace c74::min;
 using namespace c74::max;
 
-class jit_mo_gen : public object, matrix_operator {
+class jit_mo_gen : public object<jit_mo_gen>, matrix_operator {
 public:
 	
-	inlet	input	= { this, "(matrix) Input", "matrix" };
 	outlet	output	= { this, "(matrix) Output", "matrix" };
 	
 	
 	jit_mo_gen(atoms args) {
         name = symbol_unique();
-        attributes["name"]->label = gensym("Name");
+        //attributes["name"]->label = gensym("Name");
     }
 	~jit_mo_gen() {}
-
 	
-	// TODO: mode attr for how to handle the edges
-	
+    void setup(atoms args) {
+        object_register(gensym("jitter"), (symbol)name, args[0]);
+    }
 
 	ATTRIBUTE (name, symbol, _jit_sym_nothing) {
+		//void *job = max_jit_obex_jitob_get(maxobj);
+	}
+	END
+    
+    ATTRIBUTE (gentype, symbol, gensym("sin")) {
 		
 	}
 	END
-
+    
+    ATTRIBUTE (amp, double, 1.0) {
+		
+	}
+	END
 	
+    ATTRIBUTE (freq, double, 1.0) {
+		
+	}
+	END
+    
+    ATTRIBUTE (phase, double, 0.0) {
+		
+	}
+	END
+    
 	template<class matrix_type, size_t planecount>
 	cell<matrix_type,planecount> calc_cell(cell<matrix_type,planecount> input, const matrix_info& info, matrix_coord& position) {
 		cell<matrix_type,planecount> output;
-		
+		double snorm = (double)position.x() / (double)info.out_info->dim[0];
+        snorm = snorm*2. - 1.;
+        double val = snorm * freq + phase;
+        val *= M_PI;
+        output[0] = sin(val) * amp;
 		return output;
 	}
 	
@@ -46,9 +68,9 @@ private:
         JPATCHLINE_CONNECT=1,
         JPATCHLINE_ORDER=2
     } t_patchline_updatetype;
-
+    
     METHOD(patchlineupdate) {
-        t_object *x = args[0];
+        /*t_object *x = args[0];
         t_object *patchline = args[1];
         long updatetype = args[2];
         t_object *src = args[3];
@@ -70,7 +92,7 @@ private:
             case JPATCHLINE_ORDER:
                 break;
             }
-        }
+        }*/
 
     }END
 
